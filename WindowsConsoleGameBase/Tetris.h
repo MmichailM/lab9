5602929904bf605405826986e4fe74d55f945dc3
Michail Markov <stud204081@vyatsu.ru>
2025-05-27 13:10:07 +0300

    Добавление полей и конструктора по умолчанию классу Tetris

--- WindowsConsoleGameBase/Tetris.h
+++ WindowsConsoleGameBase/Tetris.h
@@ -1,8 +1,14 @@
 #pragma once
 
 #include "Engine.h"
+#include "GameField.h"
 
 class Tetris : public Engine {
+	const size_t m_Width = 14;
+	const size_t m_Height = 26;
+	GameField m_GameField;
+public:
+	Tetris();
 	bool end() const {} 
 	void on_button_press(const int button) {}
 	void update(const int dt) {}
