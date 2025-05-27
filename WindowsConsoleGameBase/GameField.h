d6bd18b91ea00d0f847d390ce1465bd52b3b09ce
Michail Markov <stud204081@vyatsu.ru>
2025-05-27 14:05:20 +0300

    Создание игрового поля

--- WindowsConsoleGameBase/GameField.h
+++ WindowsConsoleGameBase/GameField.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "Engine.h"
+#include <vector>
+
+using namespace std;
 
 class GameField {
 	size_t m_Width;
 	size_t m_Height;
+	vector<vector<wchar_t>> m_Field;
 public:
 	void resize(size_t width, size_t height);
 	void render(PaintDevice& paintDevice);
