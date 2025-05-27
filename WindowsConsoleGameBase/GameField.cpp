87042e7d8b17980583e3b19afb9edbe660b7e0a2
Michail Markov <stud204081@vyatsu.ru>
2025-05-27 13:05:02 +0300

    Создание класса GameField

---
+++ WindowsConsoleGameBase/GameField.cpp
@@ -0,0 +1,6 @@
+#include "GameField.h"
+
+void GameField::resize(size_t width, size_t height) {
+	m_Width = width;
+	m_Height = height;
+}
---
+++ WindowsConsoleGameBase/GameField.h
@@ -0,0 +1,8 @@
+#pragma once
+class GameField {
+	size_t m_Width;
+	size_t m_Height;
+public:
+	void resize(size_t width, size_t height);
+};
+
