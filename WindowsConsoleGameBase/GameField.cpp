b63c3cd8cd744930cbe58bf3efd98d165206e4ea
Michail Markov <stud204081@vyatsu.ru>
2025-05-27 13:12:32 +0300

    Добавление метода render

--- WindowsConsoleGameBase/GameField.cpp
+++ WindowsConsoleGameBase/GameField.cpp
@@ -4,3 +4,8 @@ void GameField::resize(size_t width, size_t height) {
 	m_Width = width;
 	m_Height = height;
 }
+
+void GameField::render(PaintDevice& paintDevice) {
+	Vector2 v1(0, 0);
+	paintDevice.set_char(v1, 0x2550);
+}
--- WindowsConsoleGameBase/GameField.h
+++ WindowsConsoleGameBase/GameField.h
@@ -1,8 +1,11 @@
 #pragma once
+#include "Engine.h"
+
 class GameField {
 	size_t m_Width;
 	size_t m_Height;
 public:
 	void resize(size_t width, size_t height);
+	void render(PaintDevice& paintDevice);
 };
 
